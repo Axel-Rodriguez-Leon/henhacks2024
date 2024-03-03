502771eef5ceaa65b91a472530cc720961ffedf5
JuanSaq <jsaquino04@gmail.com>
2024-03-03 00:51:03 -0500

    "better" pages

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,11 @@
 const Landing = () => {
     return (
-        <div>
-      <p>Better notes with AI</p>
+        <div className = "App">
+      <header className=".App-header">
+                Better Notes with AI
+            </header>
+      <p>Notes may not always be available, so AI will proccess lectures
+         for you, so you can have a good peace of mind when studying for your course</p>
     </div>
     )
 }
