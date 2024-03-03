181a350839cce16ebdc573057347afa02e4e7c1c
Axel-Rodriguez-Leon <115195297+Axel-Rodriguez-Leon@users.noreply.github.com>
2024-03-03 11:23:02 -0500

    fix

--- backend/transcryptAndHumanNotes.py
+++ backend/transcryptAndHumanNotes.py
@@ -1,7 +1,7 @@
 import ollama
 ollama.pull("gemma")
 #file = open("forloopsTranscript.txt")
-with open('./lecture_transcripts/pokemonTypeMatchupsLecture.txt', 'r') as file:
+with open('./lecture_transcripts/drPepperLecturewHumanandTranscript.txt', 'r') as file:
   response = ollama.chat(
     model='gemma',
     messages=[
@@ -13,11 +13,3 @@ with open('./lecture_transcripts/pokemonTypeMatchupsLecture.txt', 'r') as file:
     ],
   )
 print(response['message']['content'])
-'''response = ollama.chat(model='gemma', messages=[
-  {
-    'role': 'user',
-    'content': 'Why',
-  },
-])
-print(response['message']['content'])
-'''
