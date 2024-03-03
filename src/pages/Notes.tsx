6a50db0c6a6f3dcaacd9e779d62436ebd19c0861
JuanSaq <jsaquino04@gmail.com>
2024-03-03 01:44:55 -0500

    Added video for lecture

--- src/pages/Notes.tsx
+++ src/pages/Notes.tsx
@@ -1,12 +1,14 @@
+import YouTubeVideo from '../YouTubeVideo';
+
 const Notes = () => {
     return (
         <div>
             <h1>Video</h1>
-            <p> insert video</p>
+            <YouTubeVideo videoId="dQw4w9WgXcQ" />
             <h2> Transcript</h2>
             <h1> View Notes</h1>
             <h2> Human Notes</h2>
-            <h3> AI Notes</h3>
+            <h2> AI Notes</h2>
         </div>
     )
 }
